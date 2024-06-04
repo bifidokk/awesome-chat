9db8d380d89d1bbda3f5414e751bb475d29d17f4
bifidokk <bifidokk@gmail.com>
2024-06-04 21:30:00 +0300

    Implement auth API endpoints

--- auth/cmd/main.go
+++ auth/cmd/main.go
@@ -1,5 +1,72 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
 
+	desc "github.com/bifidokk/awesome-chat/auth/pkg/auth_v1"
+	"github.com/brianvoe/gofakeit"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+const grpcPort = 50052
+
+type server struct {
+	desc.UnimplementedAuthV1Server
+}
+
+func main() {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterAuthV1Server(s, &server{})
+
+	log.Printf("Server listening at %v", listener.Addr())
+
+	if err = s.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
+
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("Create a new user: %v", req)
+
+	return &desc.CreateResponse{
+		Id: gofakeit.Int64(),
+	}, nil
+}
+
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("Get user: %v", req)
+
+	return &desc.GetResponse{
+		Id:        gofakeit.Int64(),
+		Name:      gofakeit.Name(),
+		Email:     gofakeit.Email(),
+		Role:      desc.Role_ROLE_USER,
+		CreatedAt: timestamppb.Now(),
+		UpdatedAt: timestamppb.Now(),
+	}, nil
+}
+
+func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+	log.Printf("Update user: %v", req)
+
+	return &emptypb.Empty{}, nil
+}
+
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Delete user: %v", req)
+
+	return &emptypb.Empty{}, nil
 }
