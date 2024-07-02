8b0dfe12e9a7f3e7915e88c7458c3d51aac47d97
bifidokk <bifidokk@gmail.com>
2024-07-02 20:35:00 +0300

    Auth service. Change import

--- auth/internal/repository/user/converter/user.go
+++ auth/internal/repository/user/converter/user.go
@@ -2,9 +2,9 @@ package converter
 
 import (
 	modelRepository "github.com/bifidokk/awesome-chat/auth/internal/repository/user/model"
+	desc "github.com/bifidokk/awesome-chat/auth/pkg/auth_v1"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
-import desc "github.com/bifidokk/awesome-chat/auth/pkg/auth_v1"
 
 func ToUserFromRepository(user *modelRepository.User) *desc.GetResponse {
 	var updatedAt *timestamppb.Timestamp
